eee245cac2c6ecc7990b5fa57745537f838958f5
Gaven Loughridge <gavenlou@gmail.com>
2021-10-18 19:04:58 -0700

    Completed react-form-controls.

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({ email: null });
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange() {
+    this.setState({ email: event.target.value });
+  }
+
+  handleSubmit() {
+    event.preventDefault();
+    console.log('e-mail:', this.state);
+  }
+
+  render() {
+    return <form onSubmit={this.handleSubmit}>
+      <label>
+        Email:
+      <input type="text" email={this.state.value} onChange={this.handleChange} placeholder="Enter e-mail here"/>
+      </label>
+      <input type="submit"/>
+    </form>;
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.querySelector('#root')
+);
