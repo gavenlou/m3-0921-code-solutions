5b16c6a561775f3e24639ad51961e0ef1869efd6
Gaven Loughridge <gavenlou@gmail.com>
2021-10-25 00:32:52 -0700

    Completed react-app-drawer-component.

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default class Drawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      menu: false
+    });
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.menu) {
+      this.setState({ menu: false });
+      document.body.style.backgroundColor = 'rgba(255, 255, 255)';
+    } else {
+      this.setState({ menu: true });
+      document.body.style.backgroundColor = 'rgba(0,0,0, 0.4)';
+    }
+  }
+
+  render() {
+    let hidden = '';
+    let hidden2 = '';
+    let shade = '';
+    if (this.state.menu) {
+      hidden2 = 'navBar';
+      hidden = 'fas fa-bars menuBttn hidden';
+      shade = 'shade';
+    } else {
+      hidden = 'fas fa-bars menuBttn';
+      hidden2 = 'navBar hidden';
+      shade = '';
+    }
+    return <div>
+        <i onClick={this.handleClick} className={hidden}></i>
+        <nav className={hidden2}>
+        <h1>Menu</h1>
+        <h3 onClick={this.handleClick}>Words</h3>
+        <h3 onClick={this.handleClick}>More Words</h3>
+        <h3 onClick={this.handleClick}>Even More Words</h3>
+      </nav>
+      <div className={shade} onClick={this.handleClick}></div>
+      </div>;
+  }
+}
