93186686d8630aec9efb8f376bcfa00d0d26a991
Gaven Loughridge <gavenlou@gmail.com>
2021-10-27 23:36:02 -0700

    Finished accordion. v_v

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,25 +3,35 @@ import React from 'react';
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ current: 'none' });
+    this.state = ({ current: null });
 
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(topic) {
-    if (this.state.current !== topic || this.state.current === null) {
-      this.setState({ current: topic });
-    } else {
+  handleClick(key) {
+    if (this.state.current === key) {
       this.setState({ current: null });
+    } else {
+      this.setState({ current: key });
     }
   }
 
+  handleClass(key) {
+    let description = 'hidden';
+    if (this.state.current === key) {
+      description = 'desc';
+    } else {
+      description = 'hidden';
+    }
+    return description;
+  }
+
   render() {
-    const topicList = this.props.topics.map(topics => {
+    const topicList = this.props.topics.map((topics, index) => {
       return (
-        <div className="container" key={topics.Name}>
-          <div className="topic" onClick={ () => this.handleClick(topics.Name)}>{topics.Name}</div>
-          <div className="desc hidden">{topics.Desc}</div>
+        <div className="container" key={index}>
+          <div className="topic" onClick={() => this.handleClick(index)}>{topics.Name}</div>
+          <div className={this.handleClass(index)}>{topics.Desc}</div>
         </div>
       );
     });
